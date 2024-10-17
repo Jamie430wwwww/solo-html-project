2506a8feb809965d35d3bff42c7a1b40ce1d4caf
Jamie430wwwww <jamie430w@gmail.com>
2024-10-17 11:45:01 -0400

    working piano

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const keys = document.querySelectorAll('.key');
 
 keys.forEach(key =>{
-    key.addEventListener('click', () => playNote(key))
+    key.addEventListener('mousedown', () => playNote(key))
 })
 
 function playNote(key){
@@ -9,4 +9,10 @@ function playNote(key){
     const noteAudio = document.getElementById(key.dataset.note)
     noteAudio.currentTime = 0
     noteAudio.play()
+    key.classList.add('active')
+    key.addEventListener( 'mouseup', () => {
+        console.log('key up')
+        key.classList.remove('active')
+        noteAudio.pause()
+    })
 }
